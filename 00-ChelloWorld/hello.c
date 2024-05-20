bee8115a8d3039103ec06c09b77065c26dd19814
Manuel Echeverría <maecheverria@frba.utn.edu.ar>
2024-05-20 19:50:14 -0300

    edit

--- 00-ChelloWorld/hello.c
+++ 00-ChelloWorld/hello.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 
-
 int main(){
-    FILE *archivo;
-    archivo = fopen ("output.txt", "w");
-    fprintf (archivo, "Hello World!");
+    printf ("Hello world!");
     return 0;
 }
